add1aba3cbaa7cbcb6919bede49281eb2f82dec2
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2021-09-30 23:42:57 +0800

    Local test passed

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # jksb-zzu
- 郑州大学每日健康申报自动化
+
+修改自[@Editi0](https://github.com/Editi0)的[jksb_sysu](https://github.com/Editi0/jksb_sysu)项目，该项目最早由 [@tomatoF](https://github.com/tomatoF) 开发。适配了 GitHub Actions，可以实现每天定时运行，并由Github向注册Github账号的邮箱发送Action的执行结果。
+
+**已可以正常运行，仍存在较大不确定性，谨慎使用**
+
+## 技术方案
+
+python+selenium+firefox。
+
+## 项目配置
+
+### 1. 生成自己的仓库
+
+点击右上角的Fork按钮，将代码fork到自己的仓库
+
+### 2. 填写账号密码
+
+2.1 在fork出来的仓库中点击最右侧的Settings，然后在左侧竖栏中选择Secrets。
+
+2.2 点击右上角的New repository secret按钮，在Name一栏填入`ID`(*注意需要大写*),在Value一栏填入你的用户号，点击Add secret保存。
+
+2.3 再次点击右上角的New repository secret按钮，在Name一栏填入``PASSWORD``(*注意需要大写*),在Value一栏填入你的密码，点击Add secret保存。
+
+### 3. 定时运行
+
+默认配置为每天 22:47 UTC *(我们这里是UTC +8，相当于6：47 a.m.)*运行。
+
+控制Github Action自动运行的文件是/.github/workfolows/jksb.yml，如需修改定时运行时间，则修改该文件的`- cron:  '47 22 * * *'`一行，修改方法可参考[该文档](https://docs.github.com/en/actions/learn-github-actions/events-that-trigger-workflows#scheduled-events)。
+
+## 免责声明
+
+使用软件过程中，发生意外造成的损失由使用者承担。如遇身体不适、或居住地址发生变化，请及时更新健康申报信息。
---
+++ jksb-zzu.py
@@ -0,0 +1,88 @@
+import os, time
+from selenium import webdriver
+from util import get_img, tgbot_send
+from retrying import retry
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+
+options = webdriver.FirefoxOptions()
+options.set_preference("security.tls.version.min",1) #https://blog.csdn.net/m0_55391269/article/details/113867365
+driver = webdriver.Firefox(executable_path=f'{os.getcwd()}/geckodriver.exe', options=options)
+print("初始化selenium driver完成")
+
+# 失败后随机 3-5s 后重试，最多 10 次
+#@retry(wait_random_min=1000, wait_random_max=3000, stop_max_attempt_number=10)
+def login():
+    print("访问登录页面")
+    driver.get("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0")
+    time.sleep(2)
+
+    print("读取用户名密码")
+    id = os.environ['ID']
+    password = os.environ['PASSWORD']
+
+    print("输入用户名密码")
+    driver.find_element_by_xpath('//*[@name="uid"]').send_keys(id)
+    driver.find_element_by_xpath('//*[@name="upw"]').send_keys(password)
+
+    # 点击登录按钮
+    print("登录信息门户")
+    try:
+        driver.find_element_by_xpath('//*[@name="smbtn"]').click()
+    except:
+        raise Exception('登陆失败')
+    time.sleep(2)
+
+    sbLink=False
+    #找出申报的iframe框架
+    try:
+        sbLink=driver.find_element_by_tag_name("iframe").get_attribute("src")
+        print(sbLink)
+    except:
+        raise Exception('未能找出申报的iframe框架')
+    return sbLink
+
+# 失败后随机 3-5s 后重试，最多 6 次
+#@retry(wait_random_min=3000, wait_random_max=5000, stop_max_attempt_number=6)
+def jksb(sbLink):
+    print('访问健康申报页面')
+    driver.get(sbLink)
+    time.sleep(2)
+
+    try:
+        driver.find_element_by_xpath('//*[@name="day6"]')
+        print('打开健康申报成功')
+    except:
+        time.sleep(10)
+        print('打开健康申报失败')
+        raise Exception('打开健康申报失败')
+
+    print("点击本人填报")
+    form = driver.find_element_by_xpath('//form')
+    form.submit()
+    time.sleep(2)
+
+    print("选择’绿码‘")
+    Select(driver.find_element_by_xpath("//select[@name='myvs_13']")).select_by_value('g')
+    time.sleep(2)
+
+    print("提交健康申报")
+    driver.find_element_by_xpath('//form').submit()
+    time.sleep(2)
+
+    print("点击’确认‘按钮")
+    driver.find_element_by_xpath('//div[text()="确认"]').click()
+    time.sleep(1)
+
+    print("完成健康申报")
+
+if __name__ == "__main__":
+    sbLink=login()
+    time.sleep(2)
+    try:
+        jksb(sbLink)
+        driver.quit()
+    except:
+        print('健康申报失败')
+        driver.quit()
+
